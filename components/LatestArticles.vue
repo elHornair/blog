acffd7937cc9ba33b6c8b66da37a0050d25d3fa2
Alain Horner <alain.hornair@gmail.com>
2021-09-27 17:42:11 +0200

    home styling

--- components/LatestArticles.vue
+++ components/LatestArticles.vue
@@ -1,12 +1,53 @@
 <template>
   <div>
-    <h2>Latest articles</h2>
+    <h2
+      class="
+        text-sm
+        font-semibold
+        text-blue-600
+        uppercase
+        tracking-wide
+        mb-2
+        mt-10
+        md:mt-20
+      "
+    >
+      Latest articles
+    </h2>
     <ol>
-      <li v-for="article in articles" :key="article.slug">
-        <NuxtLink :to="{ name: 'slug', params: { slug: article.slug } }">
-          <h3>{{ article.title }}</h3>
+      <li v-for="article in articles" :key="article.slug" class="mt-6">
+        <h3
+          class="
+            mt-5
+            sm:mt-8
+            text-2xl
+            leading-8
+            font-extrabold
+            tracking-tight
+            text-gray-900
+            sm:text-3xl
+          "
+        >
+          {{ article.title }}
+        </h3>
+        <nuxt-content
+          :document="{ body: article.excerpt }"
+          class="mt-2 text-md text-gray-800"
+        />
+
+        <NuxtLink
+          :to="{ name: 'slug', params: { slug: article.slug } }"
+          class="
+            inline-block
+            mt-2
+            text-blue-600
+            font-medium
+            text-md
+            hover:text-blue-500
+          "
+        >
+          Read more
         </NuxtLink>
-        <nuxt-content :document="{ body: article.excerpt }" />
       </li>
     </ol>
   </div>
