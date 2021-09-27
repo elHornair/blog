4a88b522bea936ac39b1056664aefab54611bd27
Alain Horner <alain.hornair@gmail.com>
2021-09-27 18:46:30 +0200

    introducing tailwind typography, detail page styling

--- pages/_slug.vue
+++ pages/_slug.vue
@@ -1,13 +1,35 @@
 <template>
-  <div>
-    <NuxtLink to="/"> Go home </NuxtLink>
-    <article>
-      <h1>{{ article.title }}</h1>
-      <p>Created at: {{ formatDate(article.createdAt) }}</p>
-      <p>Updated at: {{ formatDate(article.updatedAt) }}</p>
-      <nuxt-content :document="article" />
-    </article>
-  </div>
+  <article>
+    <h1
+      class="
+        text-4xl
+        tracking-tight
+        font-extrabold
+        text-gray-900
+        sm:text-5xl
+        md:text-6xl
+      "
+    >
+      <span class="block xl:inline">{{ article.title }}</span>
+      <span class="block text-blue-800 xl:inline">{{ article.title2 }}</span>
+    </h1>
+
+    <p
+      class="
+        mt-3
+        text-base text-gray-500
+        sm:mt-5 sm:text-lg sm:mx-auto
+        md:mt-5 md:text-xl
+        lg:mx-0
+      "
+    >
+      {{ formatDate(article.createdAt) }}
+    </p>
+    <nuxt-content
+      :document="article"
+      class="prose lg:prose-lg mt-10 sm:mt-18"
+    />
+  </article>
 </template>
 
 <script>
