d40213022c412998f0318d82cd3f3effcbeb01b6
Alain Horner <alain.hornair@gmail.com>
2021-09-29 17:36:15 +0200

    article pages max-width, misc layout fixes

--- pages/_slug.vue
+++ pages/_slug.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <nav class="mb-8 sm:mb-12 md:mb-16">
+    <nav class="mb-8 sm:mb-12 md:mb-16 max-w-prose mx-auto">
       <NuxtLink to="/" class="content-link"> Home </NuxtLink>
       / <NuxtLink to="articles" class="content-link"> Articles </NuxtLink>
       /
@@ -8,50 +8,52 @@
     </nav>
 
     <article>
-      <h1
-        class="
-          text-4xl
-          tracking-tight
-          font-extrabold
-          text-gray-900
-          sm:text-5xl
-          md:text-6xl
-        "
-      >
-        <span class="block xl:inline">{{ article.title }}</span>
-        <span class="block text-blue-800 xl:inline">{{ article.title2 }}</span>
-      </h1>
-
-      <p
-        class="
-          mt-3
-          text-base text-gray-600
-          sm:mt-5 sm:text-lg sm:mx-auto
-          md:mt-5 md:text-xl
-          lg:mx-0
-        "
-      >
-        {{ article.summary }}
-      </p>
-
-      <p
-        class="
-          mt-3
-          text-base text-gray-600
-          sm:mt-5 sm:text-lg sm:mx-auto
-          md:mt-5 md:text-xl
-          lg:mx-0
-        "
-      >
-        {{ formatDate(article.publishedAt) }}
-      </p>
+      <div class="max-w-prose mx-auto">
+        <h1
+          class="
+            text-4xl
+            tracking-tight
+            font-extrabold
+            text-gray-900
+            sm:text-5xl
+            md:text-6xl
+          "
+        >
+          <span class="block xl:inline">{{ article.title }}</span>
+          <span class="block text-blue-800 xl:inline">{{
+            article.title2
+          }}</span>
+        </h1>
+        <p
+          class="
+            mt-3
+            text-base text-gray-600
+            sm:mt-5 sm:text-lg sm:mx-auto
+            md:text-xl
+            lg:mx-0
+          "
+        >
+          {{ article.summary }}
+        </p>
+        <p
+          class="
+            mt-3
+            mb-4
+            text-sm text-gray-600
+            sm:mt-5 sm:mb-6 sm:text-base
+            lg:mx-0
+          "
+        >
+          {{ formatDate(article.publishedAt) }}
+        </p>
+      </div>
       <ContentImage
         :alt="article.mainImageAlt"
         :src="article.mainImagePath"
       ></ContentImage>
       <nuxt-content
         :document="article"
-        class="prose lg:prose-lg mt-10 sm:mt-18"
+        class="prose lg:prose-lg mt-10 sm:mt-18 max-w-prose mx-auto"
       />
     </article>
   </div>
